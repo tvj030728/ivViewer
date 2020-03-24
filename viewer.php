e268379191e6b2351af4a5fce15785a0407a6047
tvj030728 <tvj030728@gmail.com>
2020-03-24 22:39:28 +0900

    Update viewer.php

--- viewer.php
+++ viewer.php
@@ -122,7 +122,7 @@ $episode = str_replace(".zip", "", $put2);
          $(window).scroll(function() {
          	var scrollHeight = $(document).height();
          	var scrollPosition = $(window).height() + $(window).scrollTop();
-         	if ($(window).scrollTop() >= $(document).height() - $(window).height() - 60) {
+         	if (scrollPosition > scrollHeight - 80) {
          		if(<?php echo $_COOKIE['auto_next'];?> == 1) {
          			if(<?php echo count($episodeselect); ?> == <?php echo $next; ?>) {
          			alert('다음화가 없습니다!');
