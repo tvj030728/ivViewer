cbd04dd58e76be64f24eb9a86116e0a97f276c1e
tvj030728 <tvj030728@gmail.com>
2020-03-26 00:53:56 +0900

    Update viewer.php

--- viewer.php
+++ viewer.php
@@ -170,21 +170,9 @@ if($type == "zip"){
          ?>
 			 <?php if (count($episodeselect) != $next): ?>
 				 <script type="text/javascript">
-				 $(window).scroll(function() {
-					var counter = 0;
- 					var counteer = 1;
-         	var scrollHeight = $(document).height();
-         	var scrollPosition = $(window).height() + $(window).scrollTop();
-         	if (scrollPosition > scrollHeight / 2) {
-         			if(<?php echo count($episodeselect); ?> == <?php echo $next; ?>) {
-         			} else {
-								var c = Number(counter) + Number(counteer);
-								if (c < 2) {
-									$.get("./system/preload.php?title=<?php echo urlencode($_GET['title']);?>&episode=<?php echo urlencode($episodeselect[$next]);?>", function(data) {});
-								}
-						 }
-         	}
-         });
+				 $(document).ready(function () {
+					 	$.get("./system/preload.php?title=<?php echo urlencode($_GET['title']);?>&episode=<?php echo urlencode($episodeselect[$next]);?>", function(data) {});
+					});
 				 </script>
 			 <?php endif; ?>
       <script>
